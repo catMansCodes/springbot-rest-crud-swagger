8325eacf9ee72de6bfcdac7d221052556d2c3865
catMansCodes <catmanscode@gmail.com>
2021-07-22 13:25:53 +0530

    First commit with project setup

---
+++ springbot-rest-crud-swagger/src/main/java/org/catmanscodes/SpringbotRestCrudSwaggerApplication.java
@@ -0,0 +1,13 @@
+package org.catmanscodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotRestCrudSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotRestCrudSwaggerApplication.class, args);
+	}
+
+}
---
+++ springbot-rest-crud-swagger/src/test/java/org/catmanscodes/SpringbotRestCrudSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.catmanscodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbotRestCrudSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
